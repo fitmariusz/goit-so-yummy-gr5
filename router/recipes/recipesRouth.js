dcd742532fddb4776502a75ce8a7e58dbd389e6b
fitmariusz <fitmariusz@gmail.com>
2024-08-05 13:23:56 +0200

    findRecipesByText

--- router/recipes/recipesRouth.js
+++ router/recipes/recipesRouth.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 // const { jwtAuth } = require("../../middlewares/jwtMiddleware");
 const { getCategiriesList } = require("../../controlers/categoriesList/categoiresListIndex");
-const { findRecipeById } = require("../../controlers/recipes/recipesIndex");
+const recipeFunction = require("../../controlers/recipes/recipesIndex");
 
 
 router.get("/category-list", getCategiriesList);
-router.get('/:id', findRecipeById)
+router.get("/search", recipeFunction.findRecipesByText);
+router.get('/:id', recipeFunction.findRecipeById);
+
 
 module.exports = router;
