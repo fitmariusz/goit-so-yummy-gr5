1f6bc316e56cfe724e76d5f404a4964afc364771
fitmariusz <fitmariusz@gmail.com>
2024-08-06 23:40:39 +0200

    add authoryzation in route

--- controlers/recipes/recipesIndex.js
+++ controlers/recipes/recipesIndex.js
@@ -27,17 +27,78 @@ const findRecipesByCategory = async (req, res, next) => {
     const recipe = await Recipe.find({ category: req.params.category }).limit(
       DEFAULT_LIMIT_PER_PAGE
     );
-    res.json(recipe);
-    // res.json(req.params.category);
+    res.json({
+      status: "success",
+      code: 200,
+      data: { recipe },
+    });
   } catch (error) {
     next(error);
   }
 };
 const insertRecipe = async (req, res, next) => {
-  res.json(req.body);
+  try {
+    const {
+      category,
+      area,
+      instructions,
+      thumb,
+      preview,
+      time,
+      favorites = null,
+      youtube = null,
+      tags = null,
+      createdAt=null,
+      updatedAt=null,
+      ingredients,
+    } = req.body;
+
+    const recipe = Recipe.create({
+      category,
+      area,
+      instructions,
+      thumb,
+      preview,
+      time,
+      favorites,
+      youtube,
+      tags,
+      createdAt,
+      updatedAt,
+      ingredients,
+    });
+
+    if (recipe) {
+      res.status(201).json({
+        status: "success",
+        code: 200,
+        data: {
+          recipe,
+        },
+      });
+    } else {
+      res.status(404).json(recipe);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 const remoweRecipe = async (req, res, next) => {
+  try {
+    const result = Recipe.findByIdAndDelete(req.params.recipeId);
+    console.log(result);
+    res.json({
+      status: "success",
+      code: "200",
+      message: "Succes delete contact",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const myRecipe = async (req, res, next) => {
   res.json(req.body);
 };
 
