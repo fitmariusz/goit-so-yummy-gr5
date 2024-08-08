31bba2201aae8694b2bbce1602b1da6e3c5cfca0
fitmariusz <fitmariusz@gmail.com>
2024-08-08 04:12:10 +0200

    start to create favorites

--- controlers/favorites/favoritesIndex.js
+++ controlers/favorites/favoritesIndex.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const Recipe = require("../../models/recipes");
 
 const addToFavorite = async (req, res, next) => {
-    if(req.params.recipeId;
   const result = await Recipe.updateOne(
-    { _id: req.params.recipeId},
-    { $push: { favorites: { $each: newFavorites } } }
+    { _id: req.params.recipeId },
+    { $push: { favorites: { $each: req.user._id } } }
   ).exec();
 };
 
